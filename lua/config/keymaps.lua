cffcdef1a7c8d77756bd63c9b21667fcd99b6b87
woarch <woarch@outlook.com>
2024-02-27 17:04:34 +0800

    add dap key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.del('n', 't');
+vim.keymap.del('n', 'T');
 
 
 -- quick command
@@ -14,3 +16,18 @@ vim.keymap.set("i", "<C-l>", "<Right>")
 vim.keymap.set("i", "<C-j>", "<Down>")
 -- quick o
 vim.keymap.set("i", "<C-o>", "<Esc>o")
+
+
+
+-- quick quit
+vim.keymap.set('n', 'q', ':q<CR>')
+
+
+-- debug
+vim.keymap.set('n', 'tb', ':DapToggleBreakPoint')
+vim.keymap.set('n', 'tB', function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+vim.keymap.set('n', 'tc', ':DapContinue')
+vim.keymap.set('n', 'ti', ':DapStepInto')
+vim.keymap.set('n', 'to', ':DapStepOut')
+vim.keymap.set('n', 'tO', ':DapStepOver')
+vim.keymap.set('n', 'tu', function() require("dapui").toggle({ }) end)
--- lua/plugins/trans.lua
+++ lua/plugins/trans.lua
@@ -6,10 +6,10 @@ return {
     end,
     keys = {
       -- 可以换成其他你想映射的键
-      { "tt", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = " Translate" },
-      { "tp", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
+      { "<leader>tt", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = " Translate" },
+      { "<leader>tp", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
       -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
-      { "ti", "<Cmd>TranslateInput<CR>", desc = " Translate From Input" },
+      { "<leader>ti", "<Cmd>TranslateInput<CR>", desc = " Translate From Input" },
     },
     dependencies = { "kkharji/sqlite.lua" },
     opts = {
