68def11de55c63618d7d080d4aa67daeaba8b598
woarch <woarch@outlook.com>
2024-03-21 11:25:04 +0800

    ext: 快捷键调整，取消翻译插件

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,32 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del('n', 't');
-vim.keymap.del('n', 'T');
-
-
 -- quick command
 vim.keymap.set("n", ";", ":")
--- left cursor on insert
-vim.keymap.set("i", "<C-h>", "<Left>")
--- right cursor on insert
-vim.keymap.set("i", "<C-l>", "<Right>")
--- quick down
-vim.keymap.set("i", "<C-j>", "<Down>")
--- quick o
-vim.keymap.set("i", "<C-o>", "<Esc>o")
-
-
 
 -- quick quit
-vim.keymap.set('n', 'q', '<cmd>q<CR>')
+vim.keymap.set("n", "q", "<cmd>q<CR>")
 
+-- Do not remove matching parentheses
+vim.keymap.set("i", "<BS>", "<C-g>u<BS>")
 
--- debug
-vim.keymap.set('n', 'tb', function() require("dap").toggle_breakpoint() end)
-vim.keymap.set('n', 'tB', function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
-vim.keymap.set('n', 'tc', function() require("dap").continue() end)
-vim.keymap.set('n', 'ti', function() require("dap").step_into() end)
-vim.keymap.set('n', 'to', function() require("dap").step_out() end)
-vim.keymap.set('n', 'tO', function() require("dap").step_over() end)
-vim.keymap.set('n', 'tu', function() require("dapui").toggle({ }) end)
+-- Do not auto complete
+vim.keymap.set("i", "'", "'")
--- lua/plugins/customs.lua
+++ lua/plugins/customs.lua
@@ -1,23 +1,2 @@
 return {
-  {
-    "JuanZoran/Trans.nvim",
-    build = function()
-      require("Trans").install()
-    end,
-    keys = {
-      -- 可以换成其他你想映射的键
-      { "<leader>tt", mode = { "n", "x" }, "<Cmd>Translate<CR>", desc = " Translate" },
-      { "<leader>tp", mode = { "n", "x" }, "<Cmd>TransPlay<CR>", desc = " Auto Play" },
-      -- 目前这个功能的视窗还没有做好，可以在配置里将view.i改成hover
-      { "<leader>ti", "<Cmd>TranslateInput<CR>", desc = " Translate From Input" },
-    },
-    dependencies = { "kkharji/sqlite.lua" },
-    opts = {
-      -- frontend = {
-      --   default = {
-      --     auto_play = false,
-      --   },
-      -- },
-    },
-  },
 }
